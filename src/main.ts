30091dd92a4ab06cb322f626eb8b8dc68e063c55
jkh675 <rluo2018@gmail.com>
2024-11-26 21:35:40 +0800

    Clean up main function legacy code

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,10 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-	// const protoBufferPackages = JSON.parse(
-	// 	(
-	// 		await readFile(join(process.cwd(), "protoBufferPackages.json"))
-	// 	).toString(),
-	// );
+	const app = await NestFactory.create(AppModule, {
+		logger: ["error", "debug", "fatal", "log", "verbose", "warn"],
+	});
 
-	// const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-	// 	AppModule,
-	// 	{
-	// 		logger: ["debug", "error", "fatal", "log", "verbose", "warn"],
-	// 		transport: Transport.GRPC,
-	// 		options: {
-	// 			package: protoBufferPackages.map((pkg) => pkg[0]),
-	// 			protoPath: protoBufferPackages.map((pkg) => pkg[1]),
-	// 			onLoadPackageDefinition: (pkg, server) => {
-	// 				new ReflectionService(pkg).addToServer(server);
-	// 			},
-	// 			url: "0.0.0.0:50051",
-	// 		},
-	// 	},
-	// );
-
-	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 
 	await app.listen(3000);
