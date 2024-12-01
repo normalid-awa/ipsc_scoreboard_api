b108b6082c5f7ce236880e35d39981429055ea65
jkh675 <rluo2018@gmail.com>
2024-12-01 18:34:18 +0800

    Apply police for shooter

--- src/shooters/shooters.resolver.ts
+++ src/shooters/shooters.resolver.ts
@@ -1,12 +1,15 @@
 import { Args, Int, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { Shooter } from "./shooter.entity";
 import { ShootersService } from "./shooters.service";
-import { NotFoundException } from "@nestjs/common";
+import { NotFoundException, UseGuards } from "@nestjs/common";
 import {
 	NewShooterArgs,
 	ShootersArgs,
 	UpdateShooterArgs,
 } from "./shooters.dto";
+import { JwtAuthGuard } from "src/auth/auth.guard";
+import { CheckPolicies, PoliciesGuard } from "src/casl/policies.guard";
+import { Action } from "src/casl/casl-ability.factory/casl-ability.factory";
 
 @Resolver(() => Shooter)
 export class ShootersResolver {
@@ -27,11 +30,15 @@ export class ShootersResolver {
 	}
 
 	@Mutation(() => Shooter)
+	@UseGuards(JwtAuthGuard, PoliciesGuard)
+	@CheckPolicies((ability) => ability.can(Action.Create, Shooter))
 	async createShooter(@Args() data: NewShooterArgs) {
 		return await this.shooterService.create(data);
 	}
 
 	@Mutation(() => Boolean)
+	@UseGuards(JwtAuthGuard, PoliciesGuard)
+	@CheckPolicies((ability) => ability.can(Action.Update, Shooter))
 	async updateShooter(
 		@Args("id", { type: () => Int }) id: number,
 		@Args() data: UpdateShooterArgs,
@@ -40,6 +47,8 @@ export class ShootersResolver {
 	}
 
 	@Mutation(() => Boolean)
+	@UseGuards(JwtAuthGuard, PoliciesGuard)
+	@CheckPolicies((ability) => ability.can(Action.Delete, Shooter))
 	async removeShooter(@Args("id", { type: () => Int }) id: number) {
 		return await this.shooterService.remove(id);
 	}
