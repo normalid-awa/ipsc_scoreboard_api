08852d5ba68811dfbd46103792542454dada5169
jkh675 <rluo2018@gmail.com>
2024-12-01 19:08:47 +0800

    Fix CaslModule not being import correctly

--- src/shooters/shooters.module.ts
+++ src/shooters/shooters.module.ts
@@ -3,9 +3,10 @@ import { ShootersResolver } from "./shooters.resolver";
 import { ShootersService } from "./shooters.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Shooter } from "./shooter.entity";
+import { CaslModule } from "src/casl/casl.module";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Shooter])],
+	imports: [TypeOrmModule.forFeature([Shooter]), CaslModule],
 	providers: [ShootersResolver, ShootersService],
 })
 export class ShootersModule {}
